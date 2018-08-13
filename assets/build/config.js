532ef55287b49cfb9ef6419222ed09ab35384476
braveworks <eclairseven@gmail.com>
2018-08-13 15:19:12 +0900

    update gsap2.0.1

--- assets/build/config.js
+++ assets/build/config.js
@@ -1,43 +1,53 @@
-const path = require('path');
-const { argv } = require('yargs');
-const merge = require('webpack-merge');
+const path = require('path')
+const { argv } = require('yargs')
+const merge = require('webpack-merge')
 
-const desire = require('./util/desire');
+const desire = require('./util/desire')
 
-const userConfig = merge(desire(`${__dirname}/../config`), desire(`${__dirname}/../config-local`));
+const userConfig = merge(
+  desire(`${__dirname}/../config`),
+  desire(`${__dirname}/../config-local`)
+)
 
-const isProduction = !!((argv.env && argv.env.production) || argv.p);
-const rootPath = (userConfig.paths && userConfig.paths.root)
-  ? userConfig.paths.root
-  : process.cwd();
+const isProduction = !!((argv.env && argv.env.production) || argv.p)
+const rootPath =
+  userConfig.paths && userConfig.paths.root
+    ? userConfig.paths.root
+    : process.cwd()
 
-const config = merge({
-  open: true,
-  copy: 'images/**/*',
-  proxyUrl: 'http://localhost:3000',
-  cacheBusting: '[name]_[hash]',
-  paths: {
-    root: rootPath,
-    assets: path.join(rootPath, 'assets'),
-    dist: path.join(rootPath, 'dist'),
+const config = merge(
+  {
+    open: true,
+    copy: 'images/**/*',
+    proxyUrl: 'http://localhost:3000',
+    cacheBusting: '[name]_[hash]',
+    paths: {
+      root: rootPath,
+      assets: path.join(rootPath, 'assets'),
+      dist: path.join(rootPath, 'dist'),
+    },
+    enabled: {
+      sourceMaps: !isProduction,
+      optimize: isProduction,
+      cacheBusting: isProduction,
+      watcher: !!argv.watch,
+    },
+    watch: [],
   },
-  enabled: {
-    sourceMaps: !isProduction,
-    optimize: isProduction,
-    cacheBusting: isProduction,
-    watcher: !!argv.watch,
-  },
-  watch: [],
-}, userConfig);
+  userConfig
+)
 
 module.exports = merge(config, {
-  env: Object.assign({ production: isProduction, development: !isProduction }, argv.env),
+  env: Object.assign(
+    { production: isProduction, development: !isProduction },
+    argv.env
+  ),
   publicPath: `${config.publicPath}/${path.basename(config.paths.dist)}/`,
   manifest: {},
-});
+})
 
 if (process.env.NODE_ENV === undefined) {
-  process.env.NODE_ENV = isProduction ? 'production' : 'development';
+  process.env.NODE_ENV = isProduction ? 'production' : 'development'
 }
 
 /**
@@ -47,7 +57,7 @@ if (process.env.NODE_ENV === undefined) {
  *   SAGE_DIST_PATH=/wp-content/themes/sage/dist yarn build:production
  */
 if (process.env.SAGE_DIST_PATH) {
-  module.exports.publicPath = process.env.SAGE_DIST_PATH;
+  module.exports.publicPath = process.env.SAGE_DIST_PATH
 }
 
 /**
